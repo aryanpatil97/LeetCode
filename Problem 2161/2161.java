649a497f8d00ce1932d1e59720c5d384042be836
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-17 14:49:48 +0530

    Solution

--- Problem 2161/2161.java
+++ Problem 2161/2161.java
@@ -1,5 +1,29 @@
 // Solution for LeetCode 2161. Partition Array According to Given Pivot
 
-public class 2161 {
-    // ...implement your solution here...
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int res[]=new int[nums.length];
+        int i=0;
+        for(int e:nums){
+            if(e<pivot){
+            res[i]=e;
+            i++;
+    }
+}
+        for(int e:nums){
+            if(e==pivot){
+                res[i]=e;
+                i++;
+
+            }
+        }
+        for(int e:nums){
+            if(e>pivot){
+                res[i]=e;
+                i++;
+
+            }
+       }
+       return res; 
+    }
 }
