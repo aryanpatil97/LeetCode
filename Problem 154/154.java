30865307948f08887735ebc4a4b91869d0368f6f
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-18 22:01:10 +0530

    Implement solutions for Problems 154 and 189
    Added a linear search solution for finding the minimum in a rotated sorted array with duplicates (Problem 154) and an in-place array rotation solution using reversal (Problem 189). Updated and migrated problem descriptions to markdown where appropriate.

--- Problem 154/154.java
+++ Problem 154/154.java
@@ -1,5 +1,12 @@
 // Solution for LeetCode 154. Find Minimum in Rotated Sorted Array II
 
-public class 154 {
-    // ...implement your solution here...
+class Solution {
+    public int findMin(int[] nums) {
+        int min=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min)
+                min=nums[i];
+        }
+        return min;
+    }
 }
--- Problem 189/189.java
+++ Problem 189/189.java
@@ -1,5 +1,21 @@
 // Solution for LeetCode 189. Rotate Array
 
-public class 189 {
-    // ...implement your solution here...
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k%=n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    private void reverse(int[] nums,int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            ++i;
+            --j;
+        }
+    }
+
 }
