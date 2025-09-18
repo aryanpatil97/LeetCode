b5f31ee1a5cea27647cc2e57ccd902197a31c1a6
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-18 08:37:23 +0530

    Implement median of two sorted arrays solution
    Added a Solution class with a method to find the median of two sorted arrays by merging and sorting them. Also updated the problem description with details and examples.

--- Problem 4/4.java
+++ Problem 4/4.java
@@ -1,5 +1,24 @@
 // Solution for LeetCode 4. Median of Two Sorted Arrays
 
-public class 4 {
-    // ...implement your solution here...
+import java.util.Arrays;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median = 0.0;
+        double total = 0.0;
+        int[] ans = new int[nums1.length+nums2.length];
+        System.arraycopy(nums1,0,ans,0,nums1.length);
+        System.arraycopy(nums2,0,ans,nums1.length,nums2.length);
+        Arrays.sort(ans);
+        int len = ans.length;
+        if(len%2!=0){
+            double odd = ans.length/2;
+            median = ans[(int)odd];
+        }
+        else if(len%2==0){
+            int i = len/2 ;
+            total = ans[i-1]+ans[i];
+            median = total/2;
+        }
+        return median;
+    }
 }
