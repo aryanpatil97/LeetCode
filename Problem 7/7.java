2e889f2077c2525299156876ab23c0e2f32fdde3
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-19 11:26:01 +0530

    Implement reverse integer solution for LeetCode 7
    Added a Solution class with a reverse method to solve the Reverse Integer problem. The method reverses the digits of a 32-bit signed integer and returns 0 if the result overflows. Also updated the problem description with details and examples.

--- Problem 7/7.java
+++ Problem 7/7.java
@@ -1,5 +1,17 @@
 // Solution for LeetCode 7. Reverse Integer
 
-public class 7 {
-    // ...implement your solution here...
+class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        while(x!=0){
+            ans = 10*ans + (x%10);
+            x=x/10; 
+        }
+        if(ans>max||ans<min){
+            return 0;
+        }
+        return (int)ans;
+    }
 }
