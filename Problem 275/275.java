74d492557e597892359b64676a9c63e52774bb3b
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-20 22:59:29 +0530

    Implement H-Index II solution using binary search
    Added a Solution class with an efficient binary search algorithm to compute the h-index for a sorted citations array, as required by LeetCode 275. This approach ensures logarithmic time complexity.

--- Problem 275/275.java
+++ Problem 275/275.java
@@ -1,5 +1,14 @@
 // Solution for LeetCode 275. H-Index II
 
-public class 275 {
-    // ...implement your solution here...
+class Solution {
+    public int hIndex(int[] citations) {
+        int l=0;
+        int r=citations.length;
+        while(l<r){
+            int mid = (l+r+1)>>>1;
+            if(citations[citations.length-mid]>=mid) l=mid;
+            else r=mid-1;
+        }
+        return l;
+    }
 }
