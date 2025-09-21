e9ffb474af53c7ae33ad4870863d72074c71bffc
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-21 09:44:58 +0530

    Add solution for LeetCode 1887 problem
    Implemented the minPairSum method to solve the Reduction Operations to Make the Array Elements Equal problem. The solution uses frequency counting and two pointers to efficiently compute the minimum possible maximum pair sum.

--- Problem 1887/1887.java
+++ Problem 1887/1887.java
@@ -1,5 +1,33 @@
 // Solution for LeetCode 1887. Reduction Operations to Make the Array Elements Equal
 
-public class 1887 {
-    // ...implement your solution here...
+class Solution {
+    public int minPairSum(int[] nums) {
+        int max = 0;
+        for(int no: nums) max = Math.max(max, no);
+
+        int res= 0;
+        int[] freq = new int[max+1];
+        for(int no: nums) freq[no] += 1;
+
+        int left = 0, right = max;
+        while(left < right){
+            while(freq[left] == 0) left++;
+
+            while(freq[right] == 0) right--;
+
+            res = Math.max(res, left+right);
+            if(freq[right] > freq[left]){
+                freq[right] -= freq[left];
+                left++;
+            }else if(freq[right] < freq[left]){
+                freq[left] -= freq[right];
+                right--;
+            }else {
+                left++;
+                right--;
+            }
+        }
+
+        return res;
+    }
 }
