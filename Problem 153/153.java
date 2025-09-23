4dc4c31aedab2212c1ec3613159dcdf93226e813
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-23 23:38:45 +0530

    Implement findMin for rotated sorted array
    Replaces the placeholder class with a Solution class implementing the findMin method, which finds the minimum value in a rotated sorted array by iterating through the array.

--- Problem 153/153.java
+++ Problem 153/153.java
@@ -1,5 +1,12 @@
 // Solution for LeetCode 153. Find Minimum in Rotated Sorted Array
-
-public class 153 {
-    // ...implement your solution here...
+class Solution {
+    public int findMin(int[] nums) {
+        // Arrays.sort(nums);
+        // return nums[0];
+        int min =nums[0];
+        for(int i=0;i<nums.length;++i){
+            min =Math.min(min,nums[i]);
+        }
+        return min;
+    }
 }
