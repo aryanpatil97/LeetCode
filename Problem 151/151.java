10cecec0b87d09708ce1c4f1c45fd53b452f2229
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-23 23:38:34 +0530

    Implement solution for LeetCode 151: Reverse Words
    Added the Solution class with a reverseWords method to reverse the order of words in a string, handling leading/trailing spaces and multiple spaces between words.

--- Problem 151/151.java
+++ Problem 151/151.java
@@ -1,5 +1,12 @@
 // Solution for LeetCode 151. Reverse Words in a String
 
-public class 151 {
-    // ...implement your solution here...
+class Solution {
+    public String reverseWords(String s) {
+        String ans ="";
+        String[] a =s.strip().split("\\W+");
+        int l = a.length;
+        if(l==1) return a[0];
+        for(int i=l-1;i>0;--i) ans+=a[i]+" ";
+        return ans + a[0];
+    }
 }
