7168883ed74e95210382cfc1fa02894074fa376e
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-24 22:17:49 +0530

    Implement minSwapsCouples using Union-Find
    Added a Solution class for LeetCode 765 using a Union-Find data structure to efficiently compute the minimum number of swaps needed for couples to sit together. The implementation includes a custom UnionFind class to track connected components.

--- Problem 765/765.java
+++ Problem 765/765.java
@@ -1,5 +1,46 @@
 // Solution for LeetCode 765. Couples Holding Hands
 
-public class 765 {
-    // ...implement your solution here...
+class Solution {
+    public int minSwapsCouples(int[] row) {
+        int n = row.length / 2;
+        UnionFind uf = new UnionFind(n);
+
+        // For each seat-pair, connect their couples
+        for (int i = 0; i < row.length; i += 2) {
+            int c1 = row[i] / 2;
+            int c2 = row[i + 1] / 2;
+            uf.union(c1, c2);
+        }
+
+        // Count unique connected components
+        return n - uf.getCount();
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int count;
+
+    UnionFind(int n) {
+        parent = new int[n];
+        count = n;
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void union(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+            count--;
+        }
+    }
+
+    int getCount() {
+        return count;
+    }
 }
