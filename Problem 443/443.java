c501500fe040f54cd07da851f713d9c8e903d7bc
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-24 22:17:13 +0530

    Implement string compression solution for LeetCode 443
    Replaced the placeholder implementation with a working solution for the string compression problem. The new code compresses the input character array in-place and returns the new length as required by the problem statement.

--- Problem 443/443.java
+++ Problem 443/443.java
@@ -1,5 +1,20 @@
 // Solution for LeetCode 443. String Compression
 
-public class 443 {
-    // ...implement your solution here...
+class Solution {
+    public int compress(char[] chars) {
+        int ans = 0;
+        for(int i = 0;i<chars.length;){
+            char letter = chars[i];
+            int count = 0;
+            while(i<chars.length&&chars[i]==letter){
+                ++count;
+                ++i;
+            }
+            chars[ans++]=letter;
+            if(count>1){
+                for (char c : String.valueOf(count).toCharArray()) chars[ans++]=c;
+            }
+        }
+        return ans;
+    }
 }
