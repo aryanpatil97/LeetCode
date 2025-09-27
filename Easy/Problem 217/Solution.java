75940e3ac9c810d9f3727619011ba9c3ccdcee20
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-09-27 10:48:27 +0530

    Add solution for Problem 217: Contains Duplicate
    Added problem description and Java solution for LeetCode Problem 217. The solution uses a HashSet to efficiently check for duplicate values in the input array.

---
+++ Easy/Problem 217/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        return false;
+    }
+}
