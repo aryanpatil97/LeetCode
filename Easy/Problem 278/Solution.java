7e4ddb449743a7698b26131a20b6ec3fd90b3b6b
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-01 11:18:24 +0530

    Refactor binary search in firstBadVersion
    Changed search bounds from 1-based to 0-based indexing for consistency. Variable names updated for clarity and code simplified.

--- Easy/Problem 278/Solution.java
+++ Easy/Problem 278/Solution.java
@@ -1,12 +1,13 @@
 // The isBadVersion API is defined in the parent class VersionControl.
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1, right = n;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) right = mid;
-            else left = mid + 1;
+        int l = 0;
+        int r = n;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(isBadVersion(mid))r = mid;
+            else l = mid + 1;
         }
-        return left;
+        return l;
     }
 }
