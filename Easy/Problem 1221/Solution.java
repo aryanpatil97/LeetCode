4a1c3017e2943b91b032bf477e5b161ef6c5d108
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-05 09:05:22 +0530

    Add solution for Problem 1221
    Implements the balancedStringSplit method to count the maximum number of balanced substrings in a given string for Problem 1221.

---
+++ Easy/Problem 1221/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0, balance = 0;
+        for (char c : s.toCharArray()) {
+            balance += c == 'L' ? 1 : -1;
+            if (balance == 0) count++;
+        }
+        return count;
+    }
+}
