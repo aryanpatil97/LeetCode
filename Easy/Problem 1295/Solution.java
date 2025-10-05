13d069749764e77ba03f42d026e3fddac9f50952
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-05 09:06:13 +0530

    Add solution for LeetCode Problem 1295
    Implements the findNumbers method to count numbers with an even number of digits in the input array.

---
+++ Easy/Problem 1295/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int n : nums) {
+            int digits = (int)Math.log10(n) + 1;
+            if (digits % 2 == 0) count++;
+        }
+        return count;
+    }
+}
