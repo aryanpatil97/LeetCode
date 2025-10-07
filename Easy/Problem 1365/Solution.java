a6d7016caf4eaa4e6d97c61f6257d788c31e56be
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-07 10:32:16 +0530

    Add solution for Problem 1365 in Java
    Implements the smallerNumbersThanCurrent method using a counting approach to efficiently determine how many numbers are smaller than each element in the input array.

---
+++ Easy/Problem 1365/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101];
+        for (int n : nums) count[n]++;
+        for (int i = 1; i < 101; i++) count[i] += count[i-1];
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i] == 0 ? 0 : count[nums[i]-1];
+        }
+        return res;
+    }
+}
