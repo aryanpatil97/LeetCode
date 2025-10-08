747fd3b12321864416e06ad69e6c95324fd80db6
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-08 10:53:25 +0530

    Add solution for Problem 1491 in Java
    Implements the average salary excluding the minimum and maximum values for Problem 1491. The solution calculates the sum, minimum, and maximum, then computes the average accordingly.

---
+++ Easy/Problem 1491/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public double average(int[] salary) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, sum = 0;
+        for (int s : salary) {
+            min = Math.min(min, s);
+            max = Math.max(max, s);
+            sum += s;
+        }
+        return (sum - min - max) / (double)(salary.length - 2);
+    }
+}
