5f143943e6841b9013ad4f0ed5f30342a59ae1b6
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-11 09:26:22 +0530

    Add placeholder Solution.java for Problem 1494
    Added a stub Solution.java file for Problem 1494 in the Easy folder to maintain consistent project structure.

---
+++ Easy/Problem 1494/Solution.java
@@ -0,0 +1,7 @@
+// Problem 1494 is a harder problem (Parallel Courses II).
+// Placeholder simple stub for the Easy folder to keep structure consistent.
+public class Solution {
+    public int placeholder() {
+        return 0;
+    }
+}
