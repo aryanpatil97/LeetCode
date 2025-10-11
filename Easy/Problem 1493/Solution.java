b5d4cded68a937c3b71b62c923dfd39cc1940aab
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-11 09:26:13 +0530

    Add solution for Problem 1493 in Java
    Implements the longestSubarray method to find the length of the longest subarray with at most one zero removed. This uses a sliding window approach for efficient computation.

---
+++ Easy/Problem 1493/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, zeros = 0, res = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeros++;
+            while (zeros > 1) {
+                if (nums[left++] == 0) zeros--;
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
