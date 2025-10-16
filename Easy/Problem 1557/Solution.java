d3a7265f4401b69cf0c104b4fffb4c9d9a593e37
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-16 10:05:23 +0530

    Add solution for Problem 1557 in Java
    Implements the findSmallestSetOfVertices method to find the minimum set of vertices needed to reach all nodes in a directed acyclic graph.

---
+++ Easy/Problem 1557/Solution.java
@@ -0,0 +1,2 @@
+import java.util.*;
+public class Solution { public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges){ boolean[] hasIncoming=new boolean[n]; for(List<Integer> e:edges) hasIncoming[e.get(1)]=true; List<Integer> res=new ArrayList<>(); for(int i=0;i<n;i++) if(!hasIncoming[i]) res.add(i); return res; } }
