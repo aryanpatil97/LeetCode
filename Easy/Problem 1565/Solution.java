2a46c10cb75d210e04f23345d7e4712c8a48f50f
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-21 10:21:16 +0530

    Add solution for Problem 1565 in Java
    Implements the minMovesToSeat method, which sorts the seats and students arrays and calculates the minimum moves required to seat all students.

---
+++ Easy/Problem 1565/Solution.java
@@ -0,0 +1,2 @@
+import java.util.*;
+public class Solution { public int minMovesToSeat(int[] seats,int[] students){ Arrays.sort(seats); Arrays.sort(students); int res=0; for(int i=0;i<seats.length;i++) res+=Math.abs(seats[i]-students[i]); return res; } }
