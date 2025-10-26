4e32fb686c197a74d7fe5315956b7c1e315e82f5
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-26 07:45:43 +0530

    Add solution for Problem 1605 restoreMatrix
    Implements the restoreMatrix method in Solution.java to construct a matrix given row and column sums for LeetCode Problem 1605.

---
+++ Easy/Problem 1605/Solution.java
@@ -0,0 +1,1 @@
+public class Solution { public int[][] restoreMatrix(int[] rowSum,int[] colSum){ int m=rowSum.length,n=colSum.length; int[][] res=new int[m][n]; for(int i=0;i<m;i++){ for(int j=0;j<n;j++){ int v=Math.min(rowSum[i],colSum[j]); res[i][j]=v; rowSum[i]-=v; colSum[j]-=v; } } return res; } }
