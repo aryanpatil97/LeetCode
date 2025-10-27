40a2f968efd564d7910b3dc6f2053a036bffa89c
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-27 07:07:57 +0530

    Add solution for Problem 1608
    Implements the specialArray method to solve Problem 1608. The method finds an integer x such that there are exactly x elements in the array greater than or equal to x.

---
+++ Easy/Problem 1608/Solution.java
@@ -0,0 +1,1 @@
+public class Solution { public int specialArray(int[] nums){ Arrays.sort(nums); int n=nums.length; for(int x=0;x<=n;x++){ int cnt=0; for(int num:nums) if(num>=x) cnt++; if(cnt==x) return x; } return -1; } }
