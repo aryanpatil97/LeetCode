0fb8a3aa9908246f1da6818a0ec74018c35d5070
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-10-28 09:27:16 +0530

    Add solution for Problem 1611
    Implemented the minPathSum method in Solution.java to compute the minimum path sum in a grid using dynamic programming.

---
+++ Easy/Problem 1611/Solution.java
@@ -0,0 +1,1 @@
+public class Solution { public int minPathSum(int[][] grid){ int m=grid.length,n=grid[0].length; int[] dp=new int[n]; for(int i=0;i<m;i++){ for(int j=0;j<n;j++){ if(j==0) dp[j]+=grid[i][j]; else dp[j]=Math.min(dp[j],dp[j-1])+grid[i][j]; } } return dp[n-1]; } }
