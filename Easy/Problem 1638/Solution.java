c07ff98bc8fbe58955ae041732f5866d7d94f221
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-03 08:42:02 +0530

    Add solution for Problem 1638
    Implements the countSubstrings method to solve Problem 1638 by counting substrings in s and t that differ by exactly one character.

---
+++ Easy/Problem 1638/Solution.java
@@ -0,0 +1,1 @@
+public class Solution { public int countSubstrings(String s, String t){ int res=0; for(int i=0;i<s.length();i++) for(int j=0;j<t.length();j++){ int diff=0; int p1=i,p2=j; while(p1<s.length()&&p2<t.length()&&(diff<=1)){ if(s.charAt(p1++)!=t.charAt(p2++)) diff++; if(diff==1) res++; } } return res; } }
