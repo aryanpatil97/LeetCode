0b1c485d6242cf0978115f62fcfcf74afeb93739
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-05 09:44:15 +0530

    Add solution for Problem 1646
    Implements the getMaximumGenerated method to solve LeetCode Problem 1646. The solution generates the array as specified and returns the maximum value.

---
+++ Easy/Problem 1646/Solution.java
@@ -0,0 +1,1 @@
+public class Solution { public int getMaximumGenerated(int n){ if(n==0) return 0; int[] a=new int[n+1]; a[0]=0; a[1]=1; int res=1; for(int i=2;i<=n;i++){ if(i%2==0) a[i]=a[i/2]; else a[i]=a[i/2]+a[i/2+1]; res=Math.max(res,a[i]); } return res; } }
