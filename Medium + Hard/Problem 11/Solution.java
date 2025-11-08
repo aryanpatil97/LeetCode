98a39089a59521b99a6fd3b908c615de9eff8020
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-08 09:50:47 +0530

    Add solution for container with most water problem
    Implements the two-pointer approach to solve the 'Container With Most Water' problem in Solution.java.

---
+++ Medium + Hard/Problem 11/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int res = 0;
+        while (i < j) {
+            int h = Math.min(height[i], height[j]);
+            res = Math.max(res, h * (j - i));
+            if (height[i] < height[j]) i++; else j--;
+        }
+        return res;
+    }
+}
