acfd83ca7ba546391b90d7d7a1ca368a44548213
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-11 06:11:42 +0530

    Implement reductionOperations for LeetCode 1887
    Replaced the previous minPairSum implementation with reductionOperations, which calculates the minimum operations to make all array elements equal. The new approach sorts the array and counts operations based on distinct values.

--- Medium + Hard/Problem 1887/1887.java
+++ Medium + Hard/Problem 1887/1887.java
@@ -1,33 +1,17 @@
 // Solution for LeetCode 1887. Reduction Operations to Make the Array Elements Equal
 
 class Solution {
-    public int minPairSum(int[] nums) {
-        int max = 0;
-        for(int no: nums) max = Math.max(max, no);
-
-        int res= 0;
-        int[] freq = new int[max+1];
-        for(int no: nums) freq[no] += 1;
-
-        int left = 0, right = max;
-        while(left < right){
-            while(freq[left] == 0) left++;
-
-            while(freq[right] == 0) right--;
-
-            res = Math.max(res, left+right);
-            if(freq[right] > freq[left]){
-                freq[right] -= freq[left];
-                left++;
-            }else if(freq[right] < freq[left]){
-                freq[left] -= freq[right];
-                right--;
-            }else {
-                left++;
-                right--;
+    public int reductionOperations(int[] nums) {
+        java.util.Arrays.sort(nums);
+        long res = 0;
+        int n = nums.length;
+        int ops = 0; // number of distinct larger values seen so far
+        for (int i = n - 1; i > 0; --i) {
+            if (nums[i] != nums[i - 1]) {
+                ops++;
             }
+            res += ops;
         }
-
-        return res;
+        return (int) res;
     }
 }
