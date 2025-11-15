3a6ffbd3f794a297001f575400e8d280301ef8f9
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-15 23:21:27 +0530

    Add solution for Problem 1402 in Java
    Implements the maxSatisfaction method to solve Problem 1402. The algorithm sorts the input array and accumulates the maximum sum by iterating from the end, breaking when the running sum becomes negative.

---
+++ Medium + Hard/Problem 1402/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxSatisfaction(int[] s) {
+        int maxSum = 0;
+        int sum=0;
+        Arrays.sort(s);
+        for(int i=s.length-1;i>=0;--i){
+            sum+=s[i];
+            if(sum<0){
+                break;
+            }
+            maxSum+=sum;
+        }
+        return maxSum;
+    }
+}
