e2a1626456d7b59b454e225f3d83953a86dfa1f1
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-18 10:35:20 +0530

    Add h-index binary search solution
    Implements the h-index calculation using a binary search approach in Solution.java for Problem 275.

---
+++ Medium + Hard/Problem 275/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l=0;
+        int r=citations.length;
+        while(l<r){
+            int mid = (l+r+1)>>>1;
+            if(citations[citations.length-mid]>=mid) l=mid;
+            else r=mid-1;
+        }
+        return l;
+    }
+}
