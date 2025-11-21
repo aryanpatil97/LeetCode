62a28f067744786dbb4e91f18cb989003dd0b7f4
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-21 13:31:22 +0530

    Add solution for LeetCode 153 (Find Minimum in Rotated Sorted Array)
    Implements a binary search algorithm to efficiently find the minimum element in a rotated sorted array. The solution is added in Solution.java for Problem 153.

---
+++ Medium + Hard/Problem 153/Solution.java
@@ -0,0 +1,33 @@
+// Solution for LeetCode 153. Find Minimum in Rotated Sorted Array
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                // min is in right half
+                left = mid + 1;
+            } else {
+                // min is at mid or in left half
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                // min is in right half
+                left = mid + 1;
+            } else {
+                // min is at mid or in left half
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
