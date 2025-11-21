31e231d1f11b16df40cad96e345f1128afbe5d73
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-21 13:31:13 +0530

    Add solution for LeetCode Problem 1456
    Implements the maxVowels method to find the maximum number of vowels in a substring of given length using a sliding window approach. Includes helper method to check for vowels.

---
+++ Medium + Hard/Problem 1456/Solution.java
@@ -0,0 +1,52 @@
+// Solution for LeetCode 1456. Maximum Number of Vowels in a Substring of Given Length
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        char[] ch = s.toCharArray();
+        int i = 0, j = 0, cnt = 0, max = 0;
+        
+        while(i < ch.length && i - j < k){
+            if(isVowel(ch[i])) cnt++;
+            max = Math.max(max, cnt);
+            i++;
+        }
+
+        while(i < ch.length){
+            if(isVowel(ch[j++])) cnt--;
+            if(isVowel(ch[i])) cnt++;
+            max = Math.max(max, cnt);
+            i++;
+        }
+        return max;
+
+    }
+    private boolean isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        return false;
+    }
+}
+class Solution {
+    public int maxVowels(String s, int k) {
+        char[] ch = s.toCharArray();
+        int i = 0, j = 0, cnt = 0, max = 0;
+        
+        while(i < ch.length && i - j < k){
+            if(isVowel(ch[i])) cnt++;
+            max = Math.max(max, cnt);
+            i++;
+        }
+
+        while(i < ch.length){
+            if(isVowel(ch[j++])) cnt--;
+            if(isVowel(ch[i])) cnt++;
+            max = Math.max(max, cnt);
+            i++;
+        }
+        return max;
+
+    }
+    private boolean isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        return false;
+    }
+}
