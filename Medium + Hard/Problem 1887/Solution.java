15f4293d3b4464e84c1f72cb458627e3627cda32
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-23 14:17:01 +0530

    Add solution for Problem 1887 reduction operations
    Implements the reductionOperations method for Problem 1887, which calculates the minimum number of operations to make all array elements equal by sorting and counting distinct values.

---
+++ Medium + Hard/Problem 1887/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        java.util.Arrays.sort(nums);
+        long res = 0;
+        int n = nums.length;
+        int ops = 0; // number of distinct larger values seen so far
+        for (int i = n - 1; i > 0; --i) {
+            if (nums[i] != nums[i - 1]) {
+                ops++;
+            }
+            res += ops;
+        }
+        return (int) res;
+    }
+}
+class Solution {
+    public int reductionOperations(int[] nums) {
+        java.util.Arrays.sort(nums);
+        long res = 0;
+        int n = nums.length;
+        int ops = 0; // number of distinct larger values seen so far
+        for (int i = n - 1; i > 0; --i) {
+            if (nums[i] != nums[i - 1]) {
+                ops++;
+            }
+            res += ops;
+        }
+        return (int) res;
+    }
+}
