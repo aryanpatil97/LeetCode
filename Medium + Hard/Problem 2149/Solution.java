6180e6ba89704719f17352d31fcb3cb777abe3e1
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-24 13:39:59 +0530

    Add solution for LeetCode 2149 in Java
    Implements the rearrangeArray method to reorder array elements by sign, alternating positive and negative numbers as required by the problem statement.

---
+++ Medium + Hard/Problem 2149/Solution.java
@@ -0,0 +1,34 @@
+// Solution for LeetCode 2149. Rearrange Array Elements by Sign
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+     int []ans=new int[nums.length];
+     int pos=0,neg=1;
+     for(int i:nums){
+        if(i>0){
+            ans[pos]=i;
+            pos+=2;
+        }else{
+            ans[neg]=i;
+            neg+=2;
+        }
+     }
+     return ans;
+    }
+}
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+     int []ans=new int[nums.length];
+     int pos=0,neg=1;
+     for(int i:nums){
+        if(i>0){
+            ans[pos]=i;
+            pos+=2;
+        }else{
+            ans[neg]=i;
+            neg+=2;
+        }
+     }
+     return ans;
+    }
+}
