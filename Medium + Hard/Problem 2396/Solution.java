0e26893d905f07c3f5a717d7a14ac7f34dee94ec
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-11-27 07:30:54 +0530

    Add solution for LeetCode 2396 Strictly Palindromic Number
    Implements the isStrictlyPalindromic method to check if a number is strictly palindromic in all bases from 2 to n-2. Note: The Solution class is duplicated in the file.

---
+++ Medium + Hard/Problem 2396/Solution.java
@@ -0,0 +1,36 @@
+// Solution for LeetCode 2396. Strictly Palindromic Number
+
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        for(int i = 2;i<=n-2;++i){
+            String pal = Integer.toString(n,i);
+            int l=0;
+            int r=pal.length()-1;
+            while(l<r){
+                if(pal.charAt(l)!=pal.charAt(r)){
+                    return false;
+                    }
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        for(int i = 2;i<=n-2;++i){
+            String pal = Integer.toString(n,i);
+            int l=0;
+            int r=pal.length()-1;
+            while(l<r){
+                if(pal.charAt(l)!=pal.charAt(r)){
+                    return false;
+                    }
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
