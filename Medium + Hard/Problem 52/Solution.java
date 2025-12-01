eb8b6800c965e639eda8ab4d6e2fd68a30b14822
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-12-01 11:40:39 +0530

    Add solution for LeetCode 52: N-Queens II
    Implements a backtracking algorithm with bitmask optimization to count the number of distinct N-Queens solutions. Note: The Solution class is duplicated in the file and may need cleanup.

---
+++ Medium + Hard/Problem 52/Solution.java
@@ -0,0 +1,50 @@
+// Solution for LeetCode 52. N-Queens II
+
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        // Use backtracking with bitmasks for columns and diagonals
+        backtrack(n, 0, 0, 0);
+        return count;
+    }
+
+    private void backtrack(int n, int cols, int diags, int antiDiags) {
+        if (cols == (1 << n) - 1) {
+            // all n queens placed
+            count++;
+            return;
+        }
+
+        int available = (~(cols | diags | antiDiags)) & ((1 << n) - 1);
+        while (available != 0) {
+            int pick = available & -available; // rightmost 1
+            available -= pick;
+            backtrack(n, cols | pick, (diags | pick) << 1, (antiDiags | pick) >> 1);
+        }
+    }
+}
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        // Use backtracking with bitmasks for columns and diagonals
+        backtrack(n, 0, 0, 0);
+        return count;
+    }
+
+    private void backtrack(int n, int cols, int diags, int antiDiags) {
+        if (cols == (1 << n) - 1) {
+            // all n queens placed
+            count++;
+            return;
+        }
+
+        int available = (~(cols | diags | antiDiags)) & ((1 << n) - 1);
+        while (available != 0) {
+            int pick = available & -available; // rightmost 1
+            available -= pick;
+            backtrack(n, cols | pick, (diags | pick) << 1, (antiDiags | pick) >> 1);
+        }
+    }
+}
