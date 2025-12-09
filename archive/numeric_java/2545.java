25ab4d6c177c45a2abd14de88e13f06e48a9039b
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-12-09 09:06:38 +0530

    Add solution for LeetCode 2545 in Java
    Implements the sortTheStudents method to sort students by their k-th score using Arrays.sort with a custom comparator.

---
+++ archive/numeric_java/2545.java
@@ -0,0 +1,10 @@
+// Solution for LeetCode 2545. Sort the Students by Their Kth Score
+
+import java.util.Arrays;
+
+class Solution {
+  public int[][] sortTheStudents(int[][] score, int k) {
+    Arrays.sort(score, (a, b) -> Integer.compare(b[k], a[k]));
+    return score;
+  }
+}
