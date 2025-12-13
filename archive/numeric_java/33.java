9c659824be1b7b5ce746966dd062dace0ce87c9a
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2025-12-13 11:30:56 +0530

    Add linear search solution for LeetCode 33
    Implements a simple linear search to solve the 'Search in Rotated Sorted Array' problem in archive/numeric_java/33.java.

---
+++ archive/numeric_java/33.java
@@ -0,0 +1,13 @@
+// Solution for LeetCode 33. Search in Rotated Sorted Array
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
