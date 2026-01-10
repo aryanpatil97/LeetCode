22fe22a70355705160736a02caeab225effb5aa0
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2026-01-10 10:53:29 +0530

    Add dynamic programming solution for regex matching
    Implements isMatch method to solve the regular expression matching problem using bottom-up dynamic programming. Handles '.' and '*' pattern characters according to problem requirements.

---
+++ Medium + Hard/Problem 10/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[m][n] = true;
+        for (int j = n - 1; j >= 0; j--) {
+            if (j + 1 < n && p.charAt(j + 1) == '*') {
+                dp[m][j] = dp[m][j + 2];
+            }
+        }
+        for (int i = m; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                boolean firstMatch = (i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+                if (j + 1 < n && p.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[m][0];
+    }
+}
