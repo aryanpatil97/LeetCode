4050e44841df4c4c6320d7f0d78cac01589d026e
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2026-01-16 10:14:29 +0530

    Add threeSumClosest solution for Problem 16
    Implements the threeSumClosest algorithm in Solution.java, which finds the sum of three integers in an array closest to a given target. Uses sorting and two-pointer technique for efficient computation.

---
+++ Medium + Hard/Problem 16/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int best = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n - 2; i++) {
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (Math.abs(sum - target) < Math.abs(best - target)) best = sum;
+                if (sum < target) {
+                    l++;
+                } else if (sum > target) {
+                    r--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+        return best;
+    }
+}
