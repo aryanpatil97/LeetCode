2ac2656350b014437c56018774b24defdfb0d7b2
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2026-03-04 10:24:31 +0530

    Delete Solution.java

--- Easy/Problem 1494/Solution.java
+++
@@ -1,7 +0,0 @@
-// Problem 1494 is a harder problem (Parallel Courses II).
-// Placeholder simple stub for the Easy folder to keep structure consistent.
-public class Solution {
-    public int placeholder() {
-        return 0;
-    }
-}
