ee5c8aeed1dd58d98a977d2badf5ca9b0bf8aa08
23f1000968 <23f1000968@ds.study.iitm.ac.in>
2026-03-11 06:44:52 +0530

    Delete Solution.java

--- Easy/Problem 1512/Solution.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        int[] cnt = new int[101];
-        int res = 0;
-        for (int n : nums) res += cnt[n]++;
-        return res;
-    }
-}
